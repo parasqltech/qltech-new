c2099d9d559ad269e27eee5f6a99b84571f1b53c
Chintan Pandya <chintanapandya@gmail.com>
2022-03-31 11:59:30 +0530

    calendly changes

--- src/pages/schedule-a-call.js
+++ src/pages/schedule-a-call.js
@@ -7,6 +7,19 @@ class Calendly extends React.Component {
     const script = document.createElement('script');
     script.setAttribute('src',  'https://assets.calendly.com/assets/external/widget.js');
     head.appendChild(script);
+	const windowUrl = window.location.search;
+	const params = new URLSearchParams(windowUrl);
+	const name ="";
+	const email ="";
+	const phone ="";
+	const curl ="https://calendly.com/qltechau/discovery-meeting";
+	if(params.length > 0){
+		name = param['name'];
+		phone = param['location'];
+		email = param['email'];
+		const curl ="https://calendly.com/qltechau/discovery-meeting/?name="+name+"&location="+phone+"&email="+email;
+	}
+	
   }
 
   componentWillUnmount() {
