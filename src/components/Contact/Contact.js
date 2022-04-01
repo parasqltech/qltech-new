3940b783c6f801b07fc3dcde22b919fabfe0866e
Chintan Pandya <chintanapandya@gmail.com>
2022-04-01 10:40:38 +0530

    marcos changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -127,7 +127,7 @@ class Contact extends React.Component{
   render (){
     return(
       <>
-      <form method="POST" id="form" action="https://script.google.com/macros/s/AKfycbwCKMtCjCKbULTmpSNqkWQNgu5FfIw-U387iryVlXsOIxPpjYyO/exec"  >
+      <form method="POST" id="form" action="https://script.google.com/macros/s/AKfycbwzMJzU5OWEENqxQOHTW3Fuy2XGUYn2dI4JHTkqE6_c7YbASMGQHCCHWl3wGpDgSmtTBw/exec"  >
 	  
 	  
 							<div className="row justify-content-center">
