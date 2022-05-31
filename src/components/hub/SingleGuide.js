b036a4f8c57f58b72d81d6c557b404371481cafe
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-05-31 12:24:44 +0530

    Update SingleGuide.js

--- src/components/hub/SingleGuide.js
+++ src/components/hub/SingleGuide.js
@@ -40,7 +40,7 @@ const SingleGuide = () => (
                                 
                                 <div className="thumbnial-content">
                                     <h2 dangerouslySetInnerHTML={{ __html: prop.node.title.replace( /(<([^>]+)>)/ig, '') }}  className="thumbnial-title"/>
-                                    <p className="thumbnial-details" dangerouslySetInnerHTML={{ __html: (prop.node.content).substring(0, 250)+"..."}} />
+                                    <p className="thumbnial-details" dangerouslySetInnerHTML={{ __html: (prop.node.content).replace( /(<([^>]+)>)/ig, '').substring(0, 250)+"..."}} />
                                     <Link to={"/guide/"+prop.node.slug+"/"} className="btn btn-secondary-link  float-left">Read More <i className="fa fa-long-arrow-right ml-1"></i></Link>
                                 </div>
                                 </div>
