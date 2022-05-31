382dd06abea81d07a3937f95aff17e0ac07416cd
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-05-31 13:23:29 +0530

    Update SingleArticle.js

--- src/components/hub/SingleArticle.js
+++ src/components/hub/SingleArticle.js
@@ -50,7 +50,7 @@ const SingleArticle = () => (
                                 </div>
                                 <div className="thumbnial-content top">
                                     <h2 dangerouslySetInnerHTML={{ __html: prop.node.title}}  className="thumbnial-title"/>
-                                    <div className="thumbnial-details" dangerouslySetInnerHTML={{ __html: (prop.node.content).substring(0, 250)+"..."  }} />
+                                    <div className="thumbnial-details" dangerouslySetInnerHTML={{ __html: (prop.node.content).replace( /(<([^>]+)>)/ig, '').substring(0, 250)+"..."  }} />
 										
                                     <Link to={"/hub/"+prop.node.categories.nodes[0].slug+"/"+prop.node.slug+"/"} className="btn btn-secondary-link  float-left">Read More <i className="fa fa-long-arrow-right ml-1"></i></Link>
                                 </div>
