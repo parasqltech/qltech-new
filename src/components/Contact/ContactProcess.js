c58d2e381f7a37a4dc6ea3e9289fbb7bd86f4b65
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-06-01 15:14:55 +0530

    Update ContactProcess.js

--- src/components/Contact/ContactProcess.js
+++ src/components/Contact/ContactProcess.js
@@ -131,7 +131,7 @@ class ContactProcess extends React.Component{
 			  'Content-Type': 'multipart/form-data'
 			  }, 
 			}).then(function(response) {
-			console.log(response);
+			window.location.href = "https://www.qltech.com.au/thank-you/";
 		    });		
 		}	
 	  } else {
