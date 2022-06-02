e03315bd28dc668ae907b14c0cf89a7fd82f14b0
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-06-02 17:53:56 +0530

    Update Header.js

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -41,7 +41,7 @@ class Header extends Component {
 		}	
 		var url_action = window.location.href;
 		init("30c4c799e1eda5b6cfe2d675f3b9e12e");
-		const last_segment = window.location.pathname.split('/').pop();
+		const last_segment = url_action.split('/');
 		console.log(last_segment);
 		const anid = cookies.get('aid');
 		const eventProperties = {
