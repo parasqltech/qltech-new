943e435adedb5a34b16056e4ac12c1db743a9cc0
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-06-03 14:06:21 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React from "react";
+import SEO from "../components/seo";
 
-import { Link } from 'gatsby'
+const browser = typeof window !== "undefined" && window;
 
-const NotFoundPage = () => (
-  <>
-	<section className="error-page bg-light text-center">
-        <div className="container">
-            <div className="main-panel">
-				<div className="row justify-content-center">
-					<div className="col-lg-10">
-						<span className="error-number">404</span>
-						<h1 className="heading-main">PAGE CAN NOT BE FOUND.</h1>
-						<p className="label-text">The page you are looking for does not exist.</p>
-						<Link to="/" className="btn-default">BACK TO HOME</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-    
-  </>
-)
-export default NotFoundPage
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <div>
+        <SEO title="404 Page not found" />
+        <h1>404 Error content...</h1>
+      </div>
+    )
+  );
+};
+
+export default NotFoundPage;
+
