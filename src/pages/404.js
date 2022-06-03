2531bca440baedb6a62ef1eb8a3d5568679a103b
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-06-03 13:00:53 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
