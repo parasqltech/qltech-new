43a4d6ff84e423d4f3eee23dc42a267f5eca9813
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-06-06 12:22:59 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react'
 import Layout from '../components/Layout'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
-class NotFoundPage extends Component {
-	render() {
-		return (
-			<Layout>
-				<h1>404: Not Found</h1>
-				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-			</Layout>
-		)
-	}
-}
-
+const NotFoundPage = () => (
+  <Layout>
+	<section className="error-page bg-light text-center">
+        <div className="container">
+            <div className="main-panel">
+				<div className="row justify-content-center">
+					<div className="col-lg-10">
+						<span className="error-number">404</span>
+						<h1 className="heading-main">PAGE CAN NOT BE FOUND.</h1>
+						<p className="label-text">The page you are looking for does not exist.</p>
+						<Link to="/" className="btn-default">BACK TO HOME</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+    
+  </Layout>
+)
 export default NotFoundPage
+
