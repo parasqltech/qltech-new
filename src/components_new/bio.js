32006b0aeb5eb672fcdac16d977d1b4f77ed4371
Kindlebit-php <kindlebit.php@gmail.com>
2022-07-15 13:38:45 +0530

    bio file in change

--- src/components_new/bio.js
+++ src/components_new/bio.js
@@ -10,7 +10,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 const Bio = () => {
   const { author } = useStaticQuery(graphql`
-    query BioQuery {
+    query BioQuer {
       # if there was more than one user, this would need to be filtered
       author: wpUser {
         firstName
