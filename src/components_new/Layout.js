90fae23c44c18abe21486cfb1d96e2488ba3d0a8
Kindlebit-php <kindlebit.php@gmail.com>
2022-07-18 21:38:51 +0530

    format change

--- src/components_new/Layout.js
+++ src/components_new/Layout.js
@@ -18,9 +18,9 @@ import "../css_new/owl.theme.default.min.css";
 import "../css_new/aos.css";
 import "../css_new/reset.min.css";
 import "../css_new/header_menu.css";
+import "../css_new/style.css";
 import "../css_new/responsive.css";
 import "../styles/bootstrap.min.css";
-import "../css_new/style.css";
 import $ from "jquery";
 import favicon from "../img/fevicon.png";
 const TemplateWrapper = ({ children }) => (
