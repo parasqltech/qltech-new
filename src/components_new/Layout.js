2b6c6fd1655d47b4f81f6fdef882b4d5b8fd5320
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-07-18 17:07:49 +0530

    Update Layout.js

--- src/components_new/Layout.js
+++ src/components_new/Layout.js
@@ -25,7 +25,10 @@ const TemplateWrapper = ({ children }) => (
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<script  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script  src={withPrefix("js/bootstrap.min.js")}></script>
+		<script  src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
 		<script  src={withPrefix("js/main.js")}></script>
+		
+		
 	</Helmet>
     <Header />
     <div>{children}</div>
