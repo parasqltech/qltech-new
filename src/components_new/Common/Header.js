2e931ed4e019b0a14d921af5b8e0772bb6f944b0
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-07-18 11:05:34 +0530

    Update Header.js

--- src/components_new/Common/Header.js
+++ src/components_new/Common/Header.js
@@ -132,7 +132,7 @@ class Header extends Component {
                     <div className="list-item">
                            <div className="con_hed_m">
 		      <a href="#" class="pd_15">
-											<h4 class="title">Platforms</h4>
+											<h4 class="title">Industries</h4>
 											<ul>
 												 <li>
 													<p class="our_hed_v"><span class="services-description mb-2">Explore possibilities and get the most from your technology stack</span></p>
