2caca6ae3162c64f88ecbc3ab615e2c21282dc50
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-07-22 12:26:15 +0530

    Update api.php

--- static/api.php
+++ static/api.php
@@ -1,1 +1,13 @@
 
+<?php  
+header("Access-Control-Allow-Origin: *");
+
+
+
+
+$fullurl = "https://maps.googleapis.com/maps/api/place/details/json?&place_id=ChIJR1mA8PyEXjkRuNlaxp48bVo&key=AIzaSyBddDWmrtLkRhg0HqWI6mACm6zSRc66HPI";
+$string .= file_get_contents($fullurl); // get json content
+$json_a = json_decode($string, true); 
+
+echo json_encode(['total' => $json_a['result']['user_ratings_total']]);
+?>
