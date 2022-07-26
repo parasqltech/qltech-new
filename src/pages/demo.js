9204599348690b8f7541863f8cd058f1f889289a
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-07-26 13:10:16 +0530

    Update demo.js

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -6,7 +6,7 @@ import Typewriter from 'typewriter-effect';
 import Tilt from 'react-parallax-tilt';
 import $ from "jquery";
 import Layout from '../components_new/Layout'
-
+import Particles from "react-tsparticles";
 import home_section from '../img/home/home-section-1-img.png';
 import home_section_bg from '../img/home/home-section-1-img-background.png';
 import about_us from '../img/about-us.png';
@@ -61,7 +61,11 @@ const IndexPage = () => (
         <meta property="twitter:description" content="QL Tech Is a Leading Digital Agency. Our Services Include Web Development, Wordpress Development, Website Design, Infusionsoft, Social Media Marketing & SEO. Get Assured Leads & Sales Ensuring amplified ROI For Your Business Through PPC, Content Marketing & More In Perth, Australia"></meta>
         </Helmet>
         
-    
+    <div className="bg">
+        <div className="area-bg">
+            <Particles options={{"particles": {"number": {"value": 80,"density": {"enable": true,"value_area": 1600}},"color": {"value": ["#fd8788", "#f1aea6", "#21a6df", "#75d3ff"]},"shape": {"type": "circle","stroke": {"width": 0,"color": "#000000",},"polygon": {"nb_sides": 5,},"image": {"src": "img/github.svg","width": 100,"height": 100}},"opacity": {"value": 0.4489553770423464,"random": false,"anim": {"enable": false,"speed": 40,"opacity_min": 0.1,"sync": false}},"size": {"value": 5,"random": true,"anim": {"enable": false,"speed": 40,"size_min": 0.1,"sync": false}},"line_linked": {"enable": false,"distance": 150,"color": "#ffffff","opacity": 0.4,"width": 1},"move": {"enable": true,"speed": 6,"direction": "none","random": false,"straight": false,"out_mode": "out","bounce": false,"attract": {"enable": false,"rotateX": 600,"rotateY": 1200}}},"interactivity": {"detect_on": "canvas","events": {"onhover": {"enable": true,"mode": "grab"},"onclick": {"enable": true,"mode": "push"},"resize": true},"modes": {"grab": {"distance": 140,"line_linked": {"opacity": 0}},"bubble": {"distance": 400,"size": 40,"duration": 2,"opacity": 8,"speed": 3},"repulse": {"distance": 200,"duration": 0.4},"push": {"particles_nb": 4},"remove": {"particles_nb": 2}}},"retina_detect": true}} />
+        </div>
+    </div>
         
      <section className="sec01 pt pb">
         <div className="container">
