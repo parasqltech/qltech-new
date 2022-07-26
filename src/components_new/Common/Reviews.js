12b328a4d2139c8e364f782095dc711086ed2513
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-07-26 10:37:32 +0530

    Update Reviews.js

--- src/components_new/Common/Reviews.js
+++ src/components_new/Common/Reviews.js
@@ -6,34 +6,21 @@ const containerStyle = {
   height: '400px'
 };
 
-const center = {lat:40.7575285, lng: -73.9884469};
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyBddDWmrtLkRhg0HqWI6mACm6zSRc66HPI",
-    libraries: ["places"]
+    googleMapsApiKey: "AIzaSyBddDWmrtLkRhg0HqWI6mACm6zSRc66HPI"
   })
 
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
-	 let service = new window.google.maps.places.PlacesService(map);
-
-service.getDetails({
-    placeId: 'ChIJAUKRDWz2wokRxngAavG2TD8'
-  }, function(place, status) {
-    console.log(place);
-  console.log(status);
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      console.log(place.user_ratings_total);
-       console.log(place.rating);
-     
-      
-      
-    }
-  })
     map.fitBounds(bounds);
     setMap(map)
   }, [])
@@ -55,4 +42,5 @@ service.getDetails({
       </GoogleMap>
   ) : <></>
 }
+
 export default MyComponent;
