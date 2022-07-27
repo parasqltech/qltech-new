859868c5fae12a47e201b1c1f456cf381e0fea91
Kindlebit-php <kindlebit.php@gmail.com>
2022-07-27 09:44:04 +0530

    google api key changed

--- static/api.php
+++ static/api.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 
 
 
-$fullurl = "https://maps.googleapis.com/maps/api/place/details/json?&place_id=ChIJR1mA8PyEXjkRuNlaxp48bVo&key=AIzaSyBddDWmrtLkRhg0HqWI6mACm6zSRc66HPI";
+$fullurl = "https://maps.googleapis.com/maps/api/place/details/json?&place_id=ChIJswUP4ym7MioRUFI2E6S8V0w&key=AIzaSyBddDWmrtLkRhg0HqWI6mACm6zSRc66HPI";
 $string .= file_get_contents($fullurl); // get json content
 $json_a = json_decode($string, true); 
 
