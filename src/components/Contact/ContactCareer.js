3629038941501fe4e8f3bb92163b544156555a1d
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-12-29 13:36:21 +0530

    Update ContactCareer.js

--- src/components/Contact/ContactCareer.js
+++ src/components/Contact/ContactCareer.js
@@ -23,6 +23,8 @@ class ContactCareer extends React.Component{
           cctc: '',
           ectc: '',
           resume: '',
+			tit: '',
+			
 		   shown_captcha: "d-none"
         
 		}
@@ -71,6 +73,9 @@ class ContactCareer extends React.Component{
 		if(flag == 1){
 			this.setState({
 				resume: e.target.value
+			});
+			this.setState({
+			tit: e.target.value
 			});
 		}
 	}
@@ -243,7 +248,7 @@ console.log(payload);
                                                 <div className="col-md-12  mb-4">
                                                     <label className="label-text">Resume Upload</label>
                                                     <div className="input-group ">
-                                                        <input type="text"  className="form-control" placeholder="Upload only pdf, docx, doc and Max file size: up to 3 MB" readOnly=""/>
+                                                        <input type="text"  className="form-control" value={this.state.tit} placeholder="Upload only pdf, docx, doc and Max file size: up to 3 MB" readOnly=""/>
                                                              <label className="input-group-btn">
                                                             <span className="btn btn-outline-secondary uploadBtn">
                                                                 Browse<input type="file" id="careerfile" value={this.state.resume} onChange={this.resume} accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document" className="d-none" required name="resume"/>  
